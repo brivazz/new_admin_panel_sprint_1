7664fe6169e54f4da088ef1be5789a597553c1cb
Sergey Toporets <yateperna@yandex.ru>
2023-03-07 13:15:07 +0300

    Спрятал secret_key в .env

--- movies_admin/config/settings.py
+++ movies_admin/config/settings.py
@@ -13,6 +13,10 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 import os
 from pathlib import Path
 
+from dotenv import load_dotenv
+
+load_dotenv()
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -21,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-^aw7wlbo&pk=6dqbz!&1i%j7ccnfnspij0d_5yqmsresl(1q5%'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
